9028b12ccf22e16d2ecd51946895bd352461c574
andrejcurkan <56604708+andrejcurkan@users.noreply.github.com>
2023-10-23 18:49:22 +0300

    Final Program.cs

--- Program.cs
+++ Program.cs
@@ -17,4 +17,29 @@ class Program
         }
     }
 
-    
+    static string[] FilterArray(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
