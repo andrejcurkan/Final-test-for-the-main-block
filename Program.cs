ece4fe35ec57385bdde3a72c679143dd18d26ac6
andrejcurkan <andrejcurkan@gmail.com>
2023-10-23 18:26:58 +0300

    first part

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива, разделенные запятыми (например, \"Hello,2,world,:-)\"): ");
+        string input = Console.ReadLine()!;
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterArray(inputArray);
+
+        Console.WriteLine("Результат:");
+        foreach (string item in resultArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    
